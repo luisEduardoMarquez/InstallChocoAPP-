cdc77872d8c1ed29faa83747e8373a47edb251bb
Luis Eduardo Marquez <49871488+lmarquez17@users.noreply.github.com>
2019-05-13 10:11:22 -0300

    Create installAPPWindows.ps1

---
+++ installAPPWindows.ps1
@@ -0,0 +1,4 @@
+﻿Set-ExecutionPolicy Bypass
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+choco install adobereader, firefox, winrar, opera 
+exit 
